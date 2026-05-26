c79b38a77d0df57631e7992d48ec98e195a49ed6
Kevin Langga <kikoirfan45@gmail.com>
2026-05-27 00:06:52 +0700

    add/ Scipio-fonts, bg-background, text-color

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,12 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import localFont from "next/font/local";
+
+
+const scipio = localFont({
+  src: "./fonts/Scipio-Regular-Exfontbff2.otf",
+  variable: "--font-scipio"
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +27,9 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${scipio.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-background">{children}</body>
     </html>
   );
 }
